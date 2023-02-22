b8d4ec5ac3c256a6d6eef7c500c1e15a64253769
Zaafir Rizwan <108717727+ZaafirRizwan@users.noreply.github.com>
2023-02-22 21:49:26 +0500

    create temp.py

---
+++ temp.py
@@ -0,0 +1,20 @@
+import numpy as np
+import torch.nn as nn
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+
+def main():
+
+
+  print("hello")
+  
+  
+  
+if __name__ == "__main__":
+
+  main()
+  
+
