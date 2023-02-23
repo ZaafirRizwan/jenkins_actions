b935bcd76abd26d69421c20f05ec77aab7a28ccb
ZaafirRizwan <ZaafirRizwan@users.noreply.github.com>
2023-02-23 16:57:50 +0000

    Apply Black formatting

--- temp2.py
+++ temp2.py
@@ -1,20 +1,15 @@
 import numpy as np
-import os 
+import os
 import pandas as pd
 import torch
 
 
-
 def main():
-  
-  a = pd.DataFrame([])
-  b = np.arange(10)
-  
-  c = os.listdir()
-  
-  
-  
-  
-  
-if __name__ == '__main__':
+    a = pd.DataFrame([])
+    b = np.arange(10)
+
+    c = os.listdir()
+
+
+if __name__ == "__main__":
     main()
