ebd5ca0c8fa7cd05feacd2428a2e559aefc7d1cf
Zaafir Rizwan <108717727+ZaafirRizwan@users.noreply.github.com>
2023-02-23 21:56:35 +0500

    Create temp2.py

---
+++ temp2.py
@@ -0,0 +1,20 @@
+import numpy as np
+import os 
+import pandas as pd
+import torch
+
+
+
+def main():
+  
+  a = pd.DataFrame([])
+  b = np.arange(10)
+  
+  c = os.listdir()
+  
+  
+  
+  
+  
+if __name__ == '__main__':
+    main()
