0b1f7edb9115460e1806ed824f4a3eba97909c51
ZaafirRizwan <ZaafirRizwan@users.noreply.github.com>
2023-02-23 16:57:51 +0000

    Apply Autoflake formatting

--- temp2.py
+++ temp2.py
@@ -1,14 +1,13 @@
 import numpy as np
 import os
 import pandas as pd
-import torch
 
 
 def main():
-    a = pd.DataFrame([])
-    b = np.arange(10)
+    pd.DataFrame([])
+    np.arange(10)
 
-    c = os.listdir()
+    os.listdir()
 
 
 if __name__ == "__main__":
